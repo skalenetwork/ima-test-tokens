8cd4f5a0b465137e806a43e1eb8c9126868b5fa5
Artem Payvin <payvinartem@gmail.com>
2022-11-17 17:42:06 +0000

    Enable blacklist

--- contracts/ERC20Blacklist.sol
+++ contracts/ERC20Blacklist.sol
@@ -168,6 +168,8 @@ contract ERC20Blacklist {
     function _transfer(address sender, address recipient, uint256 amount) internal virtual {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(!isBlackListed[sender], "ERC20: sender is blacklisted");
+        require(!isBlackListed[recipient], "ERC20: recepient is blacklisted");
 
         _balances[sender] = _balances[sender] - amount;
         _balances[recipient] = _balances[recipient] + amount;
